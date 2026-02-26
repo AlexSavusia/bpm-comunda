2df0ed58509a25f8a268c993409cd2a933d28468
Aleksei Savusya <mr.savusya@gmail.com>
2026-02-26 19:05:22 +0600

    build

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import { ProcessApi } from "./processApi";
 import { MetadataApi } from "./metadataApi";
 
 
-const baseUrl = import.meta.env.DEV ? "/api" : "https://wmunda.dev.dg-prod.su";
+const baseUrl = import.meta.env.DEV ? "/api" : "https://wmunda.dev.dg-prod.su/api";
 export const http = new HttpClient(baseUrl);
 export const processApi = new ProcessApi(http);
 export const metadataApi = new MetadataApi(http);
