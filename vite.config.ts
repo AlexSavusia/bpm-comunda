a575ffdc3d342c43067533c26d045c9a4c745054
Aleksei Savusya <mr.savusya@gmail.com>
2026-02-26 19:02:12 +0600

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
   server: {
     proxy: {
       "/api": {
@@ -13,7 +16,4 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    sourcemap: true,
-  }
 })
